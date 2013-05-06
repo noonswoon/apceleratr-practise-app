df984b87ab6c0db67fcb134d004de5eb540a44bf
Kavin Mickey Asavanant <akavin@gmail.com>
2013-05-06 17:08:53 +0700

    nothing much

--- Resources/ui/handheld/ApplicationWindow.js
+++ Resources/ui/handheld/ApplicationWindow.js
@@ -150,8 +150,8 @@ function ApplicationWindow(_userId, _userImage) {
 	};
 	Ti.App.addEventListener('openInviteFriendWindow', openInviteFriendWindowCallback);
 	
-	var TargetedModule = require('ui/handheld/Mn_CreditWindow');
-	var dummyOnBoard = new TargetedModule('');
+//	var TargetedModule = require('ui/handheld/Mn_NoInternetWindow');
+//	var dummyOnBoard = new TargetedModule('');
 		
 	var noMatchWindow = null;
 	var openNoMatchWindowCallback = function(e) {
